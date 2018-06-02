cb249f87e0a593210242649368715b46d5e3af57
Najam12 <srizvi07@uoguelph.ca>
2018-06-02 14:52:19 -0400

    updating test.h

---
+++ include/test.h
@@ -0,0 +1,18 @@
+#ifndef _Queue_API_
+#define _Queue_API_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct car{
+
+	int arrivetime;
+
+	char Direction;
+
+	char turn;
+
+} Car;
+
+
+#endif
